1ae2e43a0134e234830ba32599ae2387dbee3df8
SnsRizos <926029@unizar.es>
2025-10-29 16:41:57 +0100

    He actualizado el main y las funciones del hpp que no funcionaban correctamente

--- Práctica3/p3Main.cpp
+++ Práctica3/p3Main.cpp
@@ -6,15 +6,21 @@
 
 
 int main(){
-	evento e1;
+	evento e1, e2, e3, e4, e5;
 	crearEvento("pepe",1, e1);
 	string dato = descripcion(e1);
 	cout << dato << endl;
 	cambiarDescripcion(e1,"yets");
 	dato = descripcion(e1);
 	cout << dato << "\n\n";
-	string id1 = "aaa";
-	
+	string id1 = "aaaa";
+	crearEvento("nuevo",1, e2);
+	crearEvento("dela",1, e3);
+	crearEvento("todos",1, e4);
+	crearEvento("fiji",1, e5);
+	string id2 = "cccc";
+	string id3 = "bbbb";
+
 	colecInterdep<string,evento> bolsa;
 	crear(bolsa);
 	if(esVacia(bolsa)){
@@ -22,5 +28,8 @@ int main(){
 	}
 	cout << tamanyo(bolsa) << endl;
 	anyadirIndependiente(bolsa,id1,e1);
+	cout << tamanyo(bolsa) << endl;
+	anyadirIndependiente(bolsa,id2,e2);
+	anyadirDependiente(bolsa,id3,e2,id2);
 	cout << tamanyo(bolsa) << endl;
 }
